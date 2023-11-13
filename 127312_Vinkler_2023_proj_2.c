2f25b4d16f6e6a5fe6e5792dd8349a670b320f6a
Vinki7 <xvinklers@stuba.sk>
2023-11-13 19:03:17 +0100

    PRPR2-6 added dataloger>

--- 127312_Vinkler_2023_proj_2.c
+++ 127312_Vinkler_2023_proj_2.c
@@ -11,32 +11,12 @@ typedef struct write_list//structure definition
     int value;
     int date;
     int time;
+    
     struct write_list *next;//pointer to next element (node)
 } write_list_t;//type definition
 
 
-void n(FILE *fptr, char *id, char *pozition, char *dimension, int value, int date, int time, write_list_t *head)
-{   //function for adding new element to linked list
-    char datastorage[100];//string for storing data from file, buffer = 100 characters
-    if (head != NULL)
-    {
-        //free memory stored in write_list_t
-        write_list_t *current = head;//pointer to current element (node)
-        while (current != NULL)
-        {
-            write_list_t *next = current->next;//pointer to next element (node)
-            free(current);
-            current = next;
-        }
-    }
-    head = NULL;
-    write_list_t *new_node = malloc(sizeof(write_list_t));//allocating memory for new element
-    if (new_node == NULL)//check if memory was allocated properly
-    {
-        printf("Error allocating memory\n");
-        exit(1);//if not, end program
-    }
-}
+
 int main(void){
     printf("Zadajte príkaz:\n");
     char prikaz;
