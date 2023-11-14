f6477dec4381f6bad87091fe29b9c6a45b3dc007
Vinki7 <xvinklers@stuba.sk>
2023-11-14 15:14:16 +0100

    PRPR2-8 loading to pozition structure

--- 127312_Vinkler_2023_proj_2.c
+++ 127312_Vinkler_2023_proj_2.c
@@ -23,7 +23,7 @@ typedef struct date_time_list
     int time;
     int date;
     struct date_time_list *next;
-} t_time_list;
+} t_date_time_list;
 
 typedef struct value_list
 {
@@ -32,11 +32,11 @@ typedef struct value_list
     struct value_list *next;
 } t_value_list;
 
-void free_lists(t_id_list *id_list, t_pozition_list *pozition_list, t_time_list *time_list, t_value_list *value_list)
+void free_lists(t_id_list *id_list, t_pozition_list *pozition_list, t_date_time_list *time_list, t_value_list *value_list)
 {
     t_id_list *ptr_id_list = id_list;
     t_pozition_list *ptr_pozition_list = pozition_list;
-    t_time_list *ptr_time_list = time_list;
+    t_date_time_list *ptr_time_list = time_list;
     t_value_list *ptr_value_list = value_list; 
     while (ptr_id_list != NULL)
     {
@@ -67,7 +67,7 @@ void free_lists(t_id_list *id_list, t_pozition_list *pozition_list, t_time_list 
     }
 }
 
-void data_load(char *char_name, t_id_list **id_list, t_pozition_list **pozition_list, t_time_list **time_list, t_value_list **value_list, int *data_count){
+void data_load(char *char_name, t_id_list **id_list, t_pozition_list **pozition_list, t_date_time_list **time_list, t_value_list **value_list, int *data_count){
     FILE *fptr;
     fptr = fopen(char_name, "r");
     if (fptr == NULL)
@@ -79,11 +79,11 @@ void data_load(char *char_name, t_id_list **id_list, t_pozition_list **pozition_
     {
         free_lists(*id_list, *pozition_list, *time_list, *value_list);
     }
-    //create head of linked list
-    t_id_list *head_id_node = NULL;
-    head_id_node = (t_id_list *)malloc(sizeof(t_id_list));
-    //TODO: create head of linked lists
-    
+    //create heads of linked lists
+    t_id_list *head_id_node = (t_id_list *)malloc(sizeof(t_id_list));
+    t_pozition_list *head_pozition_node = (t_pozition_list *)malloc(sizeof(t_pozition_list));
+    t_value_list *head_value_node = (t_value_list *)malloc(sizeof(t_value_list));
+    t_date_time_list *head_time_node = (t_date_time_list *)malloc(sizeof(t_date_time_list));
     char datastorage[100];
     int line_count = 0;
     //load data to lists
@@ -106,6 +106,22 @@ void data_load(char *char_name, t_id_list **id_list, t_pozition_list **pozition_
             head_id_node = new_id_node;
             break;
         
+        case 1:
+            t_pozition_list *new_pozition_node = (t_pozition_list *)malloc(sizeof(t_pozition_list));
+            new_pozition_node->latitude = atof(&datastorage[1]);
+            if (datastorage[0] == '-')
+            {
+                new_pozition_node->latitude *= -1;
+            }
+            new_pozition_node->longitude = atof(&datastorage[9]);
+            if (datastorage[8] == '-')
+            {
+                new_pozition_node->longitude *= -1;
+            }
+            new_pozition_node->next = head_pozition_node;
+            head_pozition_node = new_pozition_node;
+            break;
+            
         default:
             break;
         }
@@ -119,7 +135,7 @@ int main(void){
     char prikaz;
     t_id_list *head_id_list = NULL;
     t_pozition_list *head_pozition_list = NULL;
-    t_time_list *head_time_list = NULL;
+    t_date_time_list *head_time_list = NULL;
     t_value_list *head_value_list = NULL;
     int data_count = 0;
     while (1)
